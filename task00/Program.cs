96ffb2066fe76f2e9d582937f9d2d926ba3c49da
texnoman <texnoman1@yandex.ru>
2022-12-16 21:59:52 +0300

    task00 end

--- task00/Program.cs
+++ task00/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// квадрат числа
+//string str = Console.ReadLine();
+//int number = Convert.ToInt32(str) расписана строка 5
+//int.number = int/parse ( Console.ReadLine()) конвертирует только строку
+Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number;
+Console.WriteLine($"Квадрат числа {number} = {square}");
