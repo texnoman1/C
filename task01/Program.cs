76826f31a0ac5c5fa0c72f1e889b80d65288d330
texnoman <texnoman1@yandex.ru>
2022-12-17 13:22:42 +0300

    task2 done

---
+++ task01/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+
+Console.WriteLine("Введите целое число 1");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое число 2");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number2 * number2 == number1)
+{
+    Console.WriteLine("Yes");
+}
+         else
+{
+    Console.WriteLine("No");
+}
+    
