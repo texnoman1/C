fa43ed06b09f295c58a52de3c3337ce4d132994a
texnoman <texnoman1@yandex.ru>
2022-12-17 19:15:42 +0300

    task02 done

---
+++ task02/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число А");
+int num_a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число Б");
+int num_b = Convert.ToInt32(Console.ReadLine());
+if (num_a < num_b)
+{
+    Console.WriteLine($"max = {num_b}");
+}
+else
+{
+    Console.WriteLine($"max = {num_a}");  
+}
