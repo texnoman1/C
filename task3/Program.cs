697b22f582a208c0e35dcac766e3ab336034c15c
texnoman <texnoman1@yandex.ru>
2022-12-17 14:03:11 +0300

    task3 end

---
+++ task3/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Введите целое число от 1 до 7");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+else if (a == 2)
+{
+    Console.WriteLine("Вторник");
+}
+else if (a == 3)
+{
+    Console.WriteLine("Среда");
+}
+if (a == 4)
+{
+    Console.WriteLine("Четверг");
+}
+else if (a == 5)
+{
+    Console.WriteLine("Пятница");
+}
+else if (a == 6)
+{
+    Console.WriteLine("Суббота");
+}
+else if (a == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+else
+{
+    Console.WriteLine("Вы ввели неверное число");
+}
